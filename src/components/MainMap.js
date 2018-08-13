36e698ae4c452eb1ba1ca01ebd0599ff5345c7b4
Dustin Ruskell <druskell@gmail.com>
2018-08-13 11:36:32 -0700

    Map is operational

---
+++ src/components/MainMap.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import GoogleMapReact from 'google-map-react';
+
+const AnyReactComponent = ({ text }) => <div>{text}</div>;
+
+class MainMap extends React.Component{
+    static defaultProps = {
+      center: {
+        lat: 59.95,
+        lng: 30.33
+      },
+      zoom: 11
+    }
+
+  render() {
+    return (
+      <div style={{ height: '100vh', width: '100%' }}>
+        <GoogleMapReact
+          bootstrapURLKeys={{ key: 'AIzaSyC7V5C9L6LbW9TKDKYSuYXKuXYYaORJrD0' }}
+          defaultCenter={this.props.center}
+          defaultZoom={this.props.zoom}
+        >
+          <AnyReactComponent
+            lat={59.955413}
+            lng={30.337844}
+            text={'Kreyser Avrora'}
+          />
+        </GoogleMapReact>
+      </div>
+    )
+  }
+}
+
+export default MainMap;
