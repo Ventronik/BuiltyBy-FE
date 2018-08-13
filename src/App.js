fa99ad68744170533390fca8ce9a99a6a1982d5c
Dustin Ruskell <druskell@gmail.com>
2018-08-13 11:23:38 -0700

    base navbar built

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Button } from 'reactstrap';
+import TopNav from './components/TopNav'
 
 class App extends Component {
+
   render() {
     return (
       <div className="App">
@@ -10,9 +13,11 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
+        <TopNav />
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <Button> HAMBRUGARZ!!!! </Button>
       </div>
     );
   }
