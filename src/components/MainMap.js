871898b1cc7c42ca2af0cdcadc57f2313dbc34dd
Dustin Ruskell <druskell@gmail.com>
2018-08-27 15:28:46 -0700

    default page now hitting the new /new route

--- src/components/MainMap.js
+++ src/components/MainMap.js
@@ -5,31 +5,6 @@ import MyGreatPlace from './Marker.js';
 // import PropTypes from 'prop-types';
 import { request } from '../helpers/index';
 
-let coords = [
-  {lat: 47.73307912, long: -122.34715996},
-  {lat: 47.61359969, long: -122.3017157},
-  {lat: 47.61274253, long: -122.34080801},
-  {lat: 47.52078359, long: -122.35876888},
-  {lat: 47.63108142, long: -122.3274716},
-  {lat: 47.54351607, long: -122.3345859},
-  {lat: 47.63631984, long: -122.32574221},
-  {lat: 47.61769425, long: -122.3335196},
-  {lat: 47.62902532, long: -122.33174325},
-  {lat: 47.61570566, long: -122.33147541},
-  {lat: 47.61504364, long: -122.33235497},
-  {lat: 47.63088912, long: -122.32820777},
-  {lat: 47.63782125, long: -122.3660894},
-  {lat: 47.61226545, long: -122.28954314},
-  {lat: 47.66203113, long: -122.37412195},
-  {lat: 47.60943195, long: -122.33937818},
-  {lat: 47.67058288, long: -122.31333693},
-  {lat: 47.60401104, long: -122.33150676},
-  {lat: 47.62296392, long: -122.3388542},
-  {lat: 47.62384901, long: -122.35575951}
-]
-
-// const AnyReactComponent = ({ text }) => <div>{text}</div>;
-
 class MainMap extends React.Component{
   constructor(props){
     super(props)
@@ -51,15 +26,15 @@ class MainMap extends React.Component{
   }
 
   getProjectsData = () =>{
-    request()
+    request('/new')
     .then(projects => {
-      this.setState({projects:JSON.parse(projects.data.replace(/\bNaN\b/g, 'null'))})
+      this.setState({projects: projects.data})
       console.log(this.state.projects)
     })
   }
 
   render() {
-    let markers = coords.map((marker, i)=> <MyGreatPlace key={i.toString()} lat={marker.lat} lng={marker.long} text={i.toString()} />)
+    let markers = this.state.projects.map((marker, i)=> <MyGreatPlace key={i.toString()} lat={marker.latitude} lng={marker.longitude} text={i.toString()} />)
     return (
       <div style={{ height: '92vh', width: '100%' }}>
         <GoogleMapReact
@@ -67,13 +42,8 @@ class MainMap extends React.Component{
           defaultCenter={this.props.center}
           defaultZoom={this.props.zoom}
         >
-          {/* <AnyReactComponent
-            lat={47.6062}
-            lng={-122.3321}
-            text={'SeattleSeattleSeattleSeattleSeattleSeattleSeattleSeattle'}
-          /> */}
           {markers}
-          <MyGreatPlace lat={47.955413} lng={-122.337844} text={'A'} />
+
         </GoogleMapReact>
       </div>
     )
