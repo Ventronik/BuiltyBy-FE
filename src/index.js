b9a552e69d14951ba761ad3c093f8780f4b5a3ec
Dustin Ruskell <druskell@gmail.com>
2018-08-31 14:57:03 -0700

    npm packages installed, skeleton written

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 import './ReactotronConfig'
 import React from 'react';
 import ReactDOM from 'react-dom';
+import store from './store';
+import { Provider } from 'react-redux';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Provider store={ store() }>
+    <App />
+  </Provider>  ,
+    document.getElementById('root'));
 registerServiceWorker();
